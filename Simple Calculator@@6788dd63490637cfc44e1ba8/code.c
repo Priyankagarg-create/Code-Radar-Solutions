e6468f125748e9c9e24174b310606332cdd1743d
Priyankagarg-create <Priyankagarg19024@gmail.com>
2025-02-18 11:33:27 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -6,13 +6,13 @@ int main(){
     scanf("%c",operator);
     scanf("%d",&y);
     switch(operator){
-    case '+':printf("%d%d%c = %d",&x,&y,operator, x+y);
+    case '+':printf("%d%d%c = %d",x,operator,y);
     break;
-    case '-':printf("%d%d%c = %d",&x,&y,operator, x-y);
+    case '-':printf("%d%d%c = %d",x,operator,y);
     break;
-    case '*':printf("%d%d%c = %d",&x,&y,operator, x*y);
+    case '*':printf("%d%d%c = %d",x,operator,y);
     break;
-    case '/':printf("%d%d%c = %d",&x,&y,operator, x/y);
+    case '/':printf("%d%d%c = %d",x,operator,y);
     break;
     default:printf("Invalid");
     return 0;
